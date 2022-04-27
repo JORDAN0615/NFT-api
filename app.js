361c0bec0aa7394d93f5a550d3bea1d780b24cf5
Swain <>
2022-04-27 21:35:51 +0800

    add an api of getting current datetime and install an pkg named moment

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const moment = require('moment');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
+
+app.get('/currentdatetime', (req, res) => {
+    res.send({
+        result: moment().format('YYYY/MM/DD HH:mm:ss'),
+    });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
